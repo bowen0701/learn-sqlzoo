293b63e5f98674f5ba7e159f50b0f9ba4ca7ecab
Bowen Li <bowen0701@gmail.com>
2018-11-27 22:57:35 +0100

    Add ex8.sql

---
+++ ex8.sql
@@ -0,0 +1,28 @@
+/* SQLZOO: 8 Using Null:
+http://sqlzoo.net/w/index.php/Using_Null */
+
+/* Ex1. List the teachers who have NULL for their department. */
+
+/* Ex2. Note the INNER JOIN misses the teachers with no department and 
+the departments with no teacher. */
+
+/* Ex3. Use a different JOIN so that all teachers are listed. */
+
+/* Ex4. Use a different JOIN so that all departments are listed. */
+
+/* Ex5. Use COALESCE to print the mobile number. Use the number '07986 444 2266' 
+if there is no number given. Show teacher name and mobile number or '07986 444 2266' */
+
+/* Ex6. Use the COALESCE function and a LEFT JOIN to print the teacher name and 
+department name. Use the string 'None' where there is no department. */
+
+/* Ex7. Use COUNT to show the number of teachers and the number of mobile phones. */
+
+/* Ex8. Use COUNT and GROUP BY dept.name to show each department and the number 
+of staff. Use a RIGHT JOIN to ensure that the Engineering department is listed. */
+
+/* Ex9. Use CASE to show the name of each teacher followed by 'Sci' if the teacher 
+is in dept 1 or 2 and 'Art' otherwise. */
+
+/* Ex10. Use CASE to show the name of each teacher followed by 'Sci' if the 
+teacher is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise. */

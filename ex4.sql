d023c11938e488f4022e627c25e5e969f7449f04
Bowen Li <bowen0701@gmail.com>
2018-11-30 22:40:39 +0100

    Revise to better query within query

--- ex4.sql
+++ ex4.sql
@@ -3,61 +3,51 @@ http://sqlzoo.net/wiki/SELECT_within_SELECT_Tutorial */
 
 /* List each country name where the population is 
 larger than that of 'Russia'. */
-SELECT name 
+SELECT name
 FROM world
 WHERE population >
-  (SELECT population 
-   FROM world
-   WHERE name = 'Russia'
-  )
+  (SELECT population FROM world WHERE name = 'Russia')
 
 /* Ex2 Show the countries in Europe with a per capita GDP 
 greater than 'United Kingdom'. */
 SELECT name
 FROM world
 WHERE continent = 'Europe'
-  AND gdp / population > (
-    SELECT gdp / population
-    FROM world
-    WHERE name = 'United Kingdom'
-    )
+  AND  gdp / population >
+    (SELECT gdp / population 
+     FROM world 
+     WHERE name = 'United Kingdom')
 
 /* Ex3. List the name and continent of countries in the continents 
 containing either Argentina or Australia. 
 Order by name of the country. */
 SELECT name, continent
 FROM world
-WHERE continent in (
-  SELECT continent
-  FROM world
-  WHERE name in ('Argentina', 'Australia')
-  )
+WHERE continent IN
+  (SELECT DISTINCT continent
+   FROM world
+   WHERE name IN ('Argentina', 'Australia'))
 ORDER BY name
 
 /* Ex4. Which country has a population that is more than Canada 
 but less than Poland? Show the name and the population. */
 SELECT name, population
 FROM world
-WHERE population > (
-  SELECT population
-  FROM world
-  WHERE name = 'Canada'
-  )
-  AND population < (
-  SELECT population
-  FROM world
-  WHERE name = 'Poland'
-  )
+WHERE 
+  population >
+    (SELECT population FROM world WHERE name = 'Canada')
+  AND population <
+    (SELECT population FROM world WHERE name = 'Poland')
 
 /* Ex5. Germany (population 80 million) has the largest population of 
 the countries in Europe. Austria (population 8.5 million) has 
 11% of the population of Germany.
 Show the name and the population of each country in Europe. 
 Show the population as a percentage of the population of Germany. */
-SELECT name, 
-       CONCAT(ROUND(100 * population / (
-         SELECT population FROM world WHERE name = 'Germany'), 0), '%'
-       ) AS population
+SELECT
+  name,
+  CONCAT(ROUND(population / 
+    (SELECT population FROM world WHERE name = 'Germany') * 100, 0), '%')
 FROM world
 WHERE continent = 'Europe'
 
@@ -70,51 +60,43 @@ WHERE gdp > ALL(
 
 /* Ex7. Find the largest country (by area) in each continent, 
 show the continent, the name and the area: */
-SELECT continent, name, area 
-FROM world x
-WHERE x.area >= ALL(
+SELECT continent, name, area
+FROM world w1
+WHERE area >= ALL(
   SELECT area 
-  FROM world y
-  WHERE y.continent = x.continent
-  )
+  FROM world w2
+  WHERE w1.continent = w2.continent 
+    AND population > 0)
 
 /* Ex8. List each continent and the name of the country that 
 comes first alphabetically. */
 SELECT continent, name
-FROM world x
-WHERE x.name <= ALL(
-  SELECT name
-  FROM world y 
-  WHERE y.continent = x.continent
-  )
+FROM world w1
+WHERE name <= ALL(
+  SELECT name 
+  FROM world w2
+  WHERE w1.continent = w2.continent)
 
 /* Ex9. Find the continents where all countries have a population <= 25000000. 
 Then find the names of the countries associated with these continents. 
 Show name, continent and population.*/
 SELECT name, continent, population
 FROM world
-WHERE continent IN (
-  SELECT continent
-  FROM world x
-  WHERE 
-    2.5E7 >= ALL(
-      SELECT population 
-      FROM world y 
-      WHERE y.continent = x.continent))
+WHERE continent IN
+  (SELECT DISTINCT continent
+   FROM world w1
+   WHERE 2.5E7 >= ALL(
+     SELECT population 
+     FROM world w2
+     WHERE w1.continent = w2.continent))
 
 /* Ex10. Some countries have populations more than three times that 
 of any of their neighbours (in the same continent). 
 Give the countries and continents. */
 SELECT name, continent
-FROM world
-WHERE name IN 
-  (SELECT name
-   FROM world x
-   WHERE x.population > 
-     3 * (SELECT MAX(y.population)
-          FROM world y
-          WHERE y.continent = x.continent
-            AND y.name <> x.name
-         )
-  )
-
+FROM world w1
+WHERE population > ALL(
+  SELECT 3 * population
+  FROM world w2
+  WHERE w1.continent = w2.continent
+    AND w1.name <> w2.name)

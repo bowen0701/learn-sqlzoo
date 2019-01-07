c69f4fccf65a333ad258f21a8e4c0d61ecc8c03d
Bowen Li <bowen0701@gmail.com>
2019-01-07 23:00:46 +0100

    Review tutor 8

--- 1_tutor8_using_null.sql
+++ 1_tutor8_using_null.sql
@@ -8,17 +8,17 @@ WHERE dept IS NULL
 
 /* Ex2. Note the INNER JOIN misses the teachers with no department and 
 the departments with no teacher. */
-SELECT t.name, d.name
-FROM teacher t INNER JOIN dept d
+SELECT t.name AS teacher, d.name AS dept
+FROM teacher t JOIN dept d
 ON t.dept = d.id
 
 /* Ex3. Use a different JOIN so that all teachers are listed. */
-SELECT t.name, d.name
+SELECT t.name AS teacher, d.name AS dept
 FROM teacher t LEFT JOIN dept d
 ON t.dept = d.id
 
 /* Ex4. Use a different JOIN so that all departments are listed. */
-SELECT t.name, d.name
+SELECT t.name AS teacher, d.name AS dept
 FROM teacher t RIGHT JOIN dept d
 ON t.dept = d.id
 
@@ -36,15 +36,13 @@ FROM teacher t LEFT JOIN dept d
 ON t.dept = d.id
 
 /* Ex7. Use COUNT to show the number of teachers and the number of mobile phones. */
-SELECT 
-  COUNT(name) AS num_teachers,
-  COUNT(mobile) AS num_mobiles
+SELECT COUNT(name), COUNT(mobile)
 FROM teacher
 
 /* Ex8. Use COUNT and GROUP BY dept.name to show each department and the number 
 of staff. Use a RIGHT JOIN to ensure that the Engineering department is listed. */
 SELECT 
-  d.name AS name_dept, 
+  d.name AS dept, 
   COUNT(t.name) AS num_teachers
 FROM teacher t RIGHT JOIN dept d
 ON t.dept = d.id
@@ -54,17 +52,17 @@ GROUP BY d.name
 is in dept 1 or 2 and 'Art' otherwise. */
 SELECT 
   name,
-  CASE WHEN dept IN ('1', '2') THEN 'Sci'
-    ELSE 'Art'
-  END AS dept_cat
+  CASE WHEN dept IN (1, 2) THEN 'Sci'
+    ELSE 'Art' 
+    END AS dept_type
 FROM teacher
 
 /* Ex10. Use CASE to show the name of each teacher followed by 'Sci' if the 
 teacher is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise. */
 SELECT
   name,
-  CASE WHEN dept IN ('1', '2') THEN 'Sci'
-    WHEN dept = '3' THEN 'Art'
+  CASE WHEN dept IN (1, 2) THEN 'Sci'
+    WHEN dept = 3 THEN 'Art'
     ELSE 'None'
-    END AS 'dep_cat'
+    END AS 'dept_type'
 FROM teacher
